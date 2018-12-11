f68c0a3f21ad5f2f688b6e80c8bb9a1988c88a2f
Alex <kurowski.dev@gmail.com>
2018-12-11 08:41:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## DEPENDENCIES
+
+- Haxe (version 4 preview 5)
+- HashLink (version 1.8)
+- `haxelib install heaps`
+- `haxelib install hlsdl`
+
+## COMPILE
+
+Run `./run` to compile a .hl file and run it in a HashLink VM.
+
+Run `./compile` to compile an executable. Make sure links in the `compile` file are correctly pointing to HashLink's .hdll files and a libhl.dylib.
---
+++ src/Game.hx
@@ -0,0 +1,74 @@
+class Game extends hxd.App {
+  var time : Float = 0;
+  var fps : h2d.Text;
+  var obj2 : h3d.scene.Mesh;
+
+  var dist = 10.;
+
+  override function init() {
+    // Events
+    function onEvent(event : hxd.Event) {
+      // trace(event.toString());
+    }
+    hxd.Window.getInstance().addEventTarget(onEvent);
+
+    // FPS text
+    fps = new h2d.Text(hxd.res.DefaultFont.get());
+    fps.text = "Hello, World!";
+    s2d.addChild(fps);
+
+    // 3d
+    var primitive = new h3d.prim.Cube();
+
+    // unindex the faces to create hard edges normals
+    primitive.unindex();
+    // add face normals
+    primitive.addNormals();
+    // add texture coordinates
+    primitive.addUVs();
+
+    var texture = new h3d.mat.Texture(1, 1);
+    var material = h3d.mat.Material.create(texture);
+
+    var obj1 = new h3d.scene.Mesh(primitive, material, s3d);
+    obj2 = new h3d.scene.Mesh(primitive, s3d);
+    obj2.material.color.setColor(0xFFB280);
+
+    var light = new h3d.scene.DirLight(new h3d.Vector(0.5, 0.5, -0.5), s3d);
+    light.enableSpecular = true;
+
+    s3d.lightSystem.ambientLight.set(0.3, 0.3, 0.3);
+
+    obj1.material.shadows = false;
+    // obj2.material.shadows = false;
+
+
+    var bounds = h3d.col.Bounds.fromValues(-0.5, -0.5, -0.5, 1.0, 1.0, 1.0);
+    var interactive = new h3d.scene.Interactive(bounds, obj2);
+
+    interactive.onOver = function (e) {
+      trace("onOver");
+      trace(e);
+    };
+
+    interactive.onOut = function (e) {
+      trace("onOut");
+      trace(e);
+    };
+  }
+
+  override function update(dt : Float) {
+    time += dt;
+
+    fps.text = "FPS: " + Std.string(1 / dt);
+
+    s3d.camera.pos.set(Math.cos(time) * dist, Math.sin(time) * dist, dist * 0.7 * Math.sin(time));
+    obj2.setRotationAxis(-0.5, 2, Math.cos(time), time + Math.PI / 2);
+
+    Sys.sleep(1/60 - dt);
+  }
+
+  static function main() {
+    new Game();
+  }
+}
